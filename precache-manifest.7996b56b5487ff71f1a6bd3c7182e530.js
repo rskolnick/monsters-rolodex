ef49cb79f82079408c517bd52696d939905d1a76
Ryan Skolnick <rhskolnick@gmail.com>
2020-02-25 14:29:44 -0500

    Updates

---
+++ precache-manifest.7996b56b5487ff71f1a6bd3c7182e530.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36c9282af3f7077313606ac949c161b9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "a4f94968dbd2f58de54b",
+    "url": "/monsters-rolodex/static/css/main.8e8aa3bc.chunk.css"
+  },
+  {
+    "revision": "3096881cff6ef734c724",
+    "url": "/monsters-rolodex/static/js/2.174b09da.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/monsters-rolodex/static/js/2.174b09da.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4f94968dbd2f58de54b",
+    "url": "/monsters-rolodex/static/js/main.cb518012.chunk.js"
+  },
+  {
+    "revision": "09dc2af6a9ad9692b985",
+    "url": "/monsters-rolodex/static/js/runtime-main.e32c56a1.js"
+  }
+]);
